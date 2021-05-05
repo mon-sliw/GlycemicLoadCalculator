da1424a670e6468eecfd7960f90ba48030791d15
nika-1993 <nika.1993@interia.pl>
2021-05-05 23:32:00 +0200

    add connection to database and initial data

---
+++ src/main/java/pl/mis/glycemicloadcalculator/entity/Product.java
@@ -0,0 +1,27 @@
+package pl.mis.glycemicloadcalculator.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "products")
+class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "english_name")
+    private String englishName;
+
+    @Column(name = "polish_name")
+    private String polishName;
+
+    @Column(name = "glycemic_index")
+    private String glycemicIndex;
+
+    @Column(name = "carbohydrate")
+    private String carbohydrate;
+}
