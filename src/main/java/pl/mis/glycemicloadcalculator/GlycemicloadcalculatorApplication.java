da889e3f00579234e5dc1b1d779c34f23b8d6239
nika-1993 <nika.1993@interia.pl>
2021-05-05 20:22:48 +0200

    Initial commit

---
+++ src/main/java/pl/mis/glycemicloadcalculator/GlycemicloadcalculatorApplication.java
@@ -0,0 +1,13 @@
+package pl.mis.glycemicloadcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlycemicloadcalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlycemicloadcalculatorApplication.class, args);
+    }
+
+}
